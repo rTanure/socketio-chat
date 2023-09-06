0d349d771a1b1f56682a29bc76b95073209c587c
Richardy Tanure <richardy.tanure@hotmail.com>
2023-09-06 20:55:05 -0300

    vercel urls

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const app = require("express")()
 const server = require("http").createServer(app)
-const io = require("socket.io")(server, {cors: {origin: "http://localhost:5173"}})
+// const io = require("socket.io")(server, {cors: {origin: "http://localhost:5173"}})
+const io = require("socket.io")(server, {cors: {origin: "https://chat.richardytanure.com/"}})
 
 const PORT = 3001
 
