9c4c2487f569f5a314f70fc7ea6174aa36b74a8c
Richardy Tanure <richardy.tanure@hotmail.com>
2023-09-06 21:15:03 -0300

    teste

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const app = require("express")()
 const server = require("http").createServer(app)
-// const io = require("socket.io")(server, {cors: {origin: "http://localhost:5173"}})
-const io = require("socket.io")(server, {cors: {origin: "https://chat.richardytanure.com/"}})
+const io = require("socket.io")(server, {cors: {origin: "http://localhost:5173"}})
+// const io = require("socket.io")(server, {cors: {origin: "https://chat.richardytanure.com/"}})
 
 const PORT = process.env.PORT || 3001
 
