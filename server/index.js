d3e16cf2a9ab4367d58435a1f4df4d48bf76e100
Richardy Tanure <richardy.tanure@hotmail.com>
2023-09-08 15:18:05 -0300

    change cors

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const app = require("express")()
 const server = require("http").createServer(app)
 const io = require('socket.io')(server, {
   cors: {
-    origin: "https://richardytanure.com",
+    origin: "chat.richardytanure.com",
     methods: ["GET", "POST"]
   }
 })
